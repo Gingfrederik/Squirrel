57eee64454051f7cf19ee41cdcf41e8a82b0e0c4
gingfrederik <jeff26912317@gmail.com>
2019-06-13 13:59:51 +0800

    change token check status code

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -14,7 +14,6 @@ import (
 func JWTAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		cfg := config.GetInstance()
-		status := true
 		r := c.Request
 		reqToken := r.Header.Get("Authorization")
 		if reqToken == "" {
@@ -29,44 +28,6 @@ func JWTAuth() gin.HandlerFunc {
 		splitToken := strings.Split(reqToken, "Bearer ")
 		tokenString := splitToken[1]
 		if tokenString == "" {
-			status = false
-		} else {
-			token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-				// Don't forget to validate the alg is what you expect:
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-				}
-
-				return []byte(cfg.SecretKey), nil
-			})
-
-			if err != nil {
-				if ve, ok := err.(*jwt.ValidationError); ok {
-					if ve.Errors&jwt.ValidationErrorExpired != 0 {
-						res := types.Response{
-							Status:  -1,
-							Message: "token expired",
-						}
-						c.JSON(http.StatusForbidden, res)
-						c.Abort()
-						return
-					} else {
-						res := types.Response{
-							Status:  -1,
-							Message: "token invaild",
-						}
-						c.JSON(http.StatusForbidden, res)
-						c.Abort()
-						return
-					}
-				}
-			}
-			if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-				c.Set("claims", token.Claims)
-			}
-		}
-
-		if !status {
 			res := types.Response{
 				Status:  -1,
 				Message: "unauthorization",
@@ -74,6 +35,40 @@ func JWTAuth() gin.HandlerFunc {
 			c.JSON(http.StatusForbidden, res)
 			c.Abort()
 			return
+		}
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return []byte(cfg.SecretKey), nil
+		})
+
+		if err != nil {
+			if ve, ok := err.(*jwt.ValidationError); ok {
+				if ve.Errors&jwt.ValidationErrorExpired != 0 {
+					res := types.Response{
+						Status:  -2,
+						Message: "token expired",
+					}
+					c.JSON(http.StatusForbidden, res)
+					c.Abort()
+					return
+				} else {
+					res := types.Response{
+						Status:  -3,
+						Message: "token invaild",
+					}
+					c.JSON(http.StatusForbidden, res)
+					c.Abort()
+					return
+				}
+			}
+		}
+
+		if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("claims", token.Claims)
 		}
 
 		c.Next()
